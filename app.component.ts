91d6561be0fd8899bb6791c4845acc8892a17f70
sara0909 <55620977+sara0909@users.noreply.github.com>
2019-10-10 12:56:39 +0530

    Add files via upload

---
+++ app.component.ts
@@ -0,0 +1,89 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator',
+  templateUrl: './calculator.component.html',
+  styleUrls: ['./calculator.component.css']
+})
+
+export class CalculatorComponent implements OnInit {
+  currentNumber = '0';
+  firstOperand = null;
+  operator = null;
+  waitForSecondNumber = false;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+}
+export class AppComponent {
+  waitForSecondNumber: any;
+  currentNumber: string;
+  firstOperand: any;
+  operator: any;
+
+  public getNumber(v: string) {
+    console.log(v);
+    if (this.waitForSecondNumber) {
+      this.currentNumber = v;
+      this.waitForSecondNumber = false;
+    } else {
+      this.currentNumber === '0' ? this.currentNumber = v : this.currentNumber += v;
+
+    }
+  }
+
+  getDecimal() {
+    if (!this.currentNumber.includes('.')) {
+        this.currentNumber += '.';
+    }
+  }
+
+  private doCalculation(op , secondOp) {
+    switch (op) {
+      case '+':
+      return this.firstOperand += secondOp;
+      case '-':
+      return this.firstOperand -= secondOp;
+      case '*':
+      return this.firstOperand *= secondOp;
+      case '/':
+      return this.firstOperand /= secondOp;
+      case '=':
+      return secondOp;
+    }
+  }
+
+  public getOperation(op: string) {
+    console.log(op);
+
+    if (this.firstOperand === null) {
+      this.firstOperand = Number(this.currentNumber);
+
+    } else if (this.operator) {
+      const result = this.doCalculation(this.operator , Number(this.currentNumber));
+      this.currentNumber = String(result);
+      this.firstOperand = result;
+    }
+    this.operator = op;
+    this.waitForSecondNumber = true;
+
+    console.log(this.firstOperand);
+
+  }
+
+  public clear() {
+    this.currentNumber = '0';
+    this.firstOperand = null;
+    this.operator = null;
+    this.waitForSecondNumber = false;
+  }
+
+  constructor() {}
+
+  // tslint:disable-next-line: use-lifecycle-interface
+  ngOnInit() {
+
+  }
+  }
+
+
